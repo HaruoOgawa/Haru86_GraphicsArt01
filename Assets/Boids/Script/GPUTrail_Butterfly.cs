b54f65712b3439335995cb5f17ea3a2e618314f7
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-08-21 13:45:03 +0900

    adjust performance

--- Assets/Boids/Script/GPUTrail_Butterfly.cs
+++ Assets/Boids/Script/GPUTrail_Butterfly.cs
@@ -25,33 +25,35 @@
 
         }
 
-        // void Update()
-        // {
+        void Update()
+        {
 
 
-        //     butterfly_trail_mat.SetBuffer("_node_data_read",butterflyTrailCalNode.buffer_node);
+            butterfly_trail_mat.SetBuffer("_node_data_read",butterflyTrailCalNode.buffer_node);
+            butterfly_trail_mat.SetBuffer("_trail_data_read",butterflyTrailCalNode.buffer_trail);
+            butterfly_trail_mat.SetFloat("_TrailWidth",trailWidth);
+            butterfly_trail_mat.SetInt("_nodeSegment",butterflyTrailCalNode.nodeSegment);
+           
+            Mesh pointMesh=new Mesh();
+            
+            Vector3[] vertex=new Vector3[1];
+            vertex[0]=new Vector3(0,0,0);
+            int[] indices=new int[1];
+            indices[0]=0;
+
+            pointMesh.vertices=vertex;
+            pointMesh.SetIndices(indices,MeshTopology.Points,0);
+            Graphics.DrawMeshInstancedProcedural(pointMesh,0,butterfly_trail_mat,new Bounds(transform.position,Vector3.one*400.0f),butterflyTrailCalNode.nodeSum);   
+        }
+
+        //   void OnRenderObject(){
+        //      butterfly_trail_mat.SetBuffer("_node_data_read",butterflyTrailCalNode.buffer_node);
         //     butterfly_trail_mat.SetBuffer("_trail_data_read",butterflyTrailCalNode.buffer_trail);
         //     butterfly_trail_mat.SetFloat("_TrailWidth",trailWidth);
         //     butterfly_trail_mat.SetInt("_nodeSegment",butterflyTrailCalNode.nodeSegment);
-        //     //butterfly_trail_mat.SetPass(0);
-        //     Mesh pointMesh=new Mesh();
-        //     Vector3[] vertex=new Vector3[1];
-        //     vertex[0]=new Vector3(0,0,0);
-        //     pointMesh.vertices=vertex;
-        //     Graphics.DrawMeshInstancedProcedural(pointMesh,0,butterfly_trail_mat,new Bounds(transform.position,Vector3.one*400.0f),butterflyTrailCalNode.nodeSum);
-        //     //Graphics.DrawProceduralNow(MeshTopology.Points,butterflyTrailCalNode.nodeSegment,butterflyTrailCalNode.nodeSum);
-
-           
+        //     butterfly_trail_mat.SetPass(0);
+        //     Graphics.DrawProceduralNow(MeshTopology.Points,butterflyTrailCalNode.nodeSegment,butterflyTrailCalNode.nodeSum);
         // }
-
-          void OnRenderObject(){
-             butterfly_trail_mat.SetBuffer("_node_data_read",butterflyTrailCalNode.buffer_node);
-              butterfly_trail_mat.SetBuffer("_trail_data_read",butterflyTrailCalNode.buffer_trail);
-            butterfly_trail_mat.SetFloat("_TrailWidth",trailWidth);
-            butterfly_trail_mat.SetInt("_nodeSegment",butterflyTrailCalNode.nodeSegment);
-            butterfly_trail_mat.SetPass(0);
-            Graphics.DrawProceduralNow(MeshTopology.Points,butterflyTrailCalNode.nodeSegment,butterflyTrailCalNode.nodeSum);
-        }
         
     }
 
