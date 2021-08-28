4bb9045ce08489e9f92cbc4e9b26eb18f578f384
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-08-28 19:54:43 +0900

    complete bezierCurve Base Script

--- Assets/L-System/Script/BezierCurve.cs
+++ Assets/L-System/Script/BezierCurve.cs
@@ -30,8 +30,9 @@ public class BezierCurve : MonoBehaviour
         
         List<Vector3> mesh_vertices=new List<Vector3>();
         List<int> mesh_indices=new List<int>();
-         List<int> mesh_triangles=new List<int>();
-        // List<Vector3> mesh_uv=new List<Vector3>();
+        List<int> mesh_triangles=new List<int>();
+        List<Vector2> mesh_uv=new List<Vector2>();
+        List<Vector3> mesh_normal=new List<Vector3>();
 
         for(int i =0;i<mesh_segment;i++){
             float t=(float)i/(float)(mesh_segment-1);
@@ -46,6 +47,8 @@ public class BezierCurve : MonoBehaviour
             Vector3 h=Vector3.Lerp(q0,q1,t);
 
             mesh_vertices.Add(h);
+
+            mesh_normal.Add(new Vector3(0,0,1f));
            
         }
 
@@ -57,46 +60,8 @@ public class BezierCurve : MonoBehaviour
 
         bezierCurveMesh.vertices=mesh_vertices.ToArray();
         bezierCurveMesh.triangles=mesh_triangles.ToArray();
+        bezierCurveMesh.normals=mesh_normal.ToArray();
+        bezierCurveMesh.RecalculateNormals();
         filter.sharedMesh=bezierCurveMesh;
     }
 }
-
-
-
-
-//       Mesh bezierCurveMesh=new Mesh();
-        
-    //     List<Vector3> mesh_vertices=new List<Vector3>();
-    //     List<int> mesh_indices=new List<int>();
-    //     // List<Vector3> mesh_triangles=new List<Vector3>();
-    //     // List<Vector3> mesh_uv=new List<Vector3>();
-
-    //     int indice=0;
-
-    //     for(int i =0;i<mesh_segment;i++){
-    //         float t=(float)i/(float)(mesh_segment-1);
-            
-    //        // Debug.Log("indice:"+indice+"/"+"t:"+t);
-    //         //Debug.Log("t:"+t);
-           
-    //         Vector3 p0=Vector3.Lerp(bezierPoints[0],bezierPoints[1],t);
-    //         Vector3 p1=Vector3.Lerp(bezierPoints[1],bezierPoints[2],t);
-    //         Vector3 p2=Vector3.Lerp(bezierPoints[2],bezierPoints[3],t);
-
-    //         Vector3 q0=Vector3.Lerp(p0,p1,t);
-    //         Vector3 q1=Vector3.Lerp(p1,p2,t);
-
-    //         Vector3 h=Vector3.Lerp(q0,q1,t);
-
-    //         mesh_vertices.Add(h);
-    //         mesh_indices.Add(indice);
-    //         if(i<mesh_segment-1){
-    //             mesh_indices.Add(indice+1);
-    //         }
-    //         indice++;
-    //     }
-
-
-    //     bezierCurveMesh.vertices=mesh_vertices.ToArray();
-    //     bezierCurveMesh.SetIndices(mesh_indices.ToArray(),MeshTopology.Lines,0);
-    //     filter.sharedMesh=bezierCurveMesh;
