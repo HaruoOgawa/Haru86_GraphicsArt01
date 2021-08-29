10010a9dc8010df94ac42ee11a64a882eeac8703
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-08-29 16:45:21 +0900

    L-System Class Base

---
+++ Assets/L-System/Script/CPU_LSystem.cs
@@ -0,0 +1,31 @@
+﻿
+namespace GraphicsArt.LSystem.CPU_LSystem{
+    
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using GraphicsArt.LSystem.CPU_LSystem_Calculator;
+
+    public class CPU_LSystem : MonoBehaviour
+    {
+        #region SerializeField
+        [SerializeField] CPU_LSystem_Calculator cpu_LSystem_Calculator;
+        #endregion
+
+        #region 
+        Mesh pointMesh;
+        #endregion
+        
+        void Start()
+        {
+            pointMesh=new Mesh();
+            Vector3[] vertex={new Vector3(0,0,0),new Vector3(1,0,0),new Vector3(1,1,0)};
+            pointMesh.vertices=vertex;
+        }
+
+        void Update()
+        {
+         
+        }
+    }
+}
---
+++ Assets/L-System/Script/CPU_LSystem_Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphicsArt.LSystem.CPU_LSystem_Calculator{
+    public struct LSystem_Data{
+            
+    }
+    public class CPU_LSystem_Calculator : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
