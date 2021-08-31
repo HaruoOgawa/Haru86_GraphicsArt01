ae5ae919e1841d5a8dbd1b38012b80af5d9a4010
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-08-31 12:10:01 +0900

    test B-Spline Curve

---
+++ Assets/L-System/Script/B_Spline_Curve.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_Spline_Curve : MonoBehaviour
+{
+    [SerializeField] MeshRenderer meshRenderer;
+    [SerializeField] MeshFilter filter;
+    [SerializeField] Material b_spline_mat;
+    [SerializeField] int meshCount=10;
+    [SerializeField] Vector3 p1=new Vector3(0,0,0);
+    [SerializeField] Vector3 p2=new Vector3(0,0,0);
+    [SerializeField] Vector3 p3=new Vector3(0,0,0);
+    void Start()
+    {
+        Mesh bSpline_mesh=new Mesh();
+        
+        List<Vector3> bspline_points=new List<Vector3>();
+        List<int> bspline_index=new List<int>();
+
+        for(int i=0;i<meshCount;i++){
+            Vector3 point=new Vector3(0,0,0);
+            float t=(int)(i/meshCount-1);
+            
+            point=CalBSpline(p1,p2,p3,t);
+            bspline_points.Add(point);
+            bspline_index.Add(i);
+        }
+
+        bSpline_mesh.vertices=bspline_points.ToArray();
+        bSpline_mesh.SetIndices(bspline_index.ToArray(),MeshTopology.Lines,0);
+
+        meshRenderer.material=b_spline_mat;
+        filter.mesh=bSpline_mesh;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    Vector3 CalBSpline(Vector2 p1,Vector2 p2,Vector2 p3,float t){
+        float x=BSplineX(p1.x,p2.x,p3.x,t);
+        float y=BSplineY(p1.y,p2.y,p3.y,t);
+        return new Vector3(x,y,0);
+    }
+
+    float BSplineX(float x1,float x2,float x3,float t){
+        return Mathf.Pow((1-t),2)*x1+2.0f*(1-t)*x2+Mathf.Pow(t,2)*x3;
+    }
+
+    float BSplineY(float y1,float y2,float y3,float t){
+         return Mathf.Pow((1-t),2)*y1+2.0f*(1-t)*y2+Mathf.Pow(t,2)*y3;
+    }
+}
--- Assets/L-System/Script/CPU_LSystem.cs
+++ Assets/L-System/Script/CPU_LSystem.cs
@@ -11,6 +11,7 @@ namespace GraphicsArt.LSystem.CPU_LSystem{
         [SerializeField] Material cpu_lsystem_mat;
         [SerializeField] float len=10.0f;
         [SerializeField] float attenuation =0.95123f;
+        [Range(1,10)]
         [SerializeField] int generations=5;
         [SerializeField] float ang=20f;
         void Start()
@@ -45,29 +46,44 @@ namespace GraphicsArt.LSystem.CPU_LSystem{
             GL.Vertex(new Vector3(0,l,0));
             GL.End();
             
+            //1///////////////////////////////////
             GL.PushMatrix();
-            Random.InitState(gene);
+            Random.InitState(gene+1*100);
             var posCurrent=current*Matrix4x4.TRS(
                 new Vector3(0,l,0),
                 Quaternion.AngleAxis((Random.value*2.0f-1.0f)*ang,Vector3.forward),
                 Vector3.one
             );
-            Random.InitState(gene*50);
+            Random.InitState(gene+1*1000);
             CalBaseLSystem(posCurrent,gene - 1,l*Random.value*0.5f+0.5f);
             GL.PopMatrix();
             
-            //2
+            //2/////////////////////////////////////
             GL.PushMatrix();
-            Random.InitState(-gene);
+            Random.InitState(gene+2*100);
             Matrix4x4 negCurrent=current*Matrix4x4.TRS(
                 new Vector3(0,l,0),
                 Quaternion.AngleAxis((Random.value*2.0f-1.0f)*ang,Vector3.forward),
                 Vector3.one
             );
-            Random.InitState(gene*50);
+            Random.InitState(gene+2*1000);
             CalBaseLSystem(negCurrent,gene - 1,l*Random.value*0.5f+0.5f);
             GL.PopMatrix();
+            
             
         }
     }
 }
+
+//  void CalNode(Matrix4x4 current,int gene,float l,int i){
+//             GL.PushMatrix();
+//             Random.InitState(gene+i*100);
+//             var posCurrent=current*Matrix4x4.TRS(
+//                 new Vector3(0,l,0),
+//                 Quaternion.AngleAxis((Random.value*2.0f-1.0f)*ang,Vector3.forward),
+//                 Vector3.one
+//             );
+//             Random.InitState(gene+i*1000);
+//             CalBaseLSystem(posCurrent,gene - 1,l*Random.value*0.5f+0.5f,i);
+//             GL.PopMatrix();
+//         }
