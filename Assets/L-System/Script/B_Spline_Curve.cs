7840b0cedd9de702c2d4b0cda0335f7d5b8d1f1c
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-09-01 10:53:01 +0900

    comp base script of B-Spline Curve

--- Assets/L-System/Script/B_Spline_Curve.cs
+++ Assets/L-System/Script/B_Spline_Curve.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class B_Spline_Curve : MonoBehaviour
 {
+    struct B_Spline_Data{
+        public Vector3 position;
+        public int index;
+
+        public B_Spline_Data(Vector3 p,int i){
+            this.position=p;
+            this.index=i;
+        }
+    }
+
     [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] MeshFilter filter;
     [SerializeField] Material b_spline_mat;
-    [SerializeField] int meshCount=10;
+    [SerializeField] int t=10;
     
     [SerializeField] List<Vector3> controlPoints=new List<Vector3>();
+    [SerializeField] float knotMin=0.0f;
+    [SerializeField] float knotMax=1.0f;
+    [SerializeField] float tWidth=0.01f;
     void Start()
     {
-      
+        Mesh B_Spline_Mesh=new Mesh();
+        B_Spline_Data[] data=Cal_BSplineCurve();
+        Vector3[] pos=new Vector3[t];
+        int[] index=new int[t];
+        for(int i=0;i<t;i++){
+            pos[i]=data[i].position;
+            index[i]=data[i].index;
+        }
+        B_Spline_Mesh.vertices=pos;
+        B_Spline_Mesh.SetIndices(index,MeshTopology.Lines,0);
+        
+        meshRenderer.material=b_spline_mat;
+        filter.mesh=B_Spline_Mesh;
     }
 
     void Update()
@@ -20,7 +45,7 @@ public class B_Spline_Curve : MonoBehaviour
         
     }
 
-    void Cal_BSplineCurve(){
+    B_Spline_Data[] Cal_BSplineCurve(){
         int p=controlPoints.Count;
         //12
         int n=3;
@@ -29,14 +54,32 @@ public class B_Spline_Curve : MonoBehaviour
         //16
 
         float[] u=GetKnotVector(m,n);
-
-        for(int i=0;i<p-1;i++){
-
+        List<float> tDelta=new List<float>();
+        int num=(int)(u[u.Length-1]/tWidth);
+        for(int i=0;i<num;i++){
+            tDelta.Add((float)(tWidth*i));
         }
+
+        //このSを頂点座標として利用する
+        B_Spline_Data[] S=new B_Spline_Data[tDelta.Count];
+        for(int i=0;i<tDelta.Count;i++){
+            S[i]=new B_Spline_Data(new Vector3(0,0,0),i);
+        }
+        
+        S[0].position=controlPoints[0];
+
+        //各TにおけるBスプラインの値を求めている
+        for(int i=1;i<tDelta.Count;i++){
+            for(int j=0;j<p;j++){
+                float b=GetBasisFunction(u,j,n,tDelta[i]);
+                S[i].position=S[i].position+controlPoints[j]*b;
+            }
+        }
+        return S;
 
     }
 
-    float[] GetKnotVector(int m,int n,float knotMin=0.0f,float knotMax=1.0f){
+    float[] GetKnotVector(int m,int n){
         List<float> knotVector=new List<float>();
         int knotN=n+1;
         //4
@@ -57,8 +100,28 @@ public class B_Spline_Curve : MonoBehaviour
         return knotVector.ToArray();
     }
 
-    void GetBasisFunction(){
+    float GetBasisFunction(float[] u,int j,int k,float t){
+        float w1=0.0f;
+        float w2=0.0f;
 
+        if(k==0){
+            if(u[j]<t&&t<=u[j+1]){
+                return 1.0f;
+            }else{
+                return 0.0f;
+            }
+        }else{
+            if(u[j+k+1]-u[j+1]!=0.0f){
+                w1=GetBasisFunction(u,j+1,k-1,t)*(u[j+k+1]-t)/(u[j+k+1]-u[j+1]);
+            }
+
+            if((u[j+k]-u[j])!=0.0f){
+                w2=GetBasisFunction(u,j,k-1,t)*(t-u[j])/(u[j+k]-u[j]);
+            }
+            
+            return w1+w2;
+        }
+       
     }
     
 }
