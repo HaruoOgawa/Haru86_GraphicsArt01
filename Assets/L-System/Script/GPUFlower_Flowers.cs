3248ff915b5c01fe67d587426d18c3401d449dc9
HaruoOgawa <haru.spring.yodai@gmail.com>
2021-09-07 09:11:48 +0900

    adjust

--- Assets/L-System/Script/GPUFlower_Flowers.cs
+++ Assets/L-System/Script/GPUFlower_Flowers.cs
@@ -12,12 +12,11 @@ using System.Runtime.InteropServices;
     [SerializeField] GPUFlower_Base gPUFlower_Base;
     [SerializeField] ComputeShader flowers_cs;
     [SerializeField] Material flowers_mat;
+    [SerializeField] PetalData[] petalDatas;
     #endregion
 
     #region private region
-    struct Flowers_data{
 
-    }
     ComputeBuffer flowers_buffer;
     Mesh flowers_mesh;
     #endregion
@@ -25,7 +24,7 @@ using System.Runtime.InteropServices;
         void Start()
         {
             Init();
-        
+            SetupFlowerdata();
         }
 
         void Update()
@@ -37,7 +36,13 @@ using System.Runtime.InteropServices;
 
         void Init(){
             flowers_mesh=new Mesh();
-            flowers_buffer=new ComputeBuffer(gPUFlower_Base.count,Marshal.SizeOf(typeof(Flowers_data)));
+            flowers_buffer=new ComputeBuffer(gPUFlower_Base.count,Marshal.SizeOf(typeof(GPUFlower_Base.BaseFlower_Data)));
+        }
+
+        void SetupFlowerdata(){
+            List<GPUFlower_Base.BaseFlower_Data> flower_data=new List<GPUFlower_Base.BaseFlower_Data>();
+            flower_data.Add(GPUFlower_Base.Cal_BSpline_Surface(petalDatas[0].controlPoints,petalDatas[0].knotMin,petalDatas[0].knotMax,petalDatas[0].tWidth));
+            flowers_buffer.SetData(flower_data.ToArray());
         }
 
     }
